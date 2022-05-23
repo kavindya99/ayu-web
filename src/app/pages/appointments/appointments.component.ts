47bbcc97facc683bfc0457fd467c5813b18e5bae
@kavindya <kavindya@ninodigitalhouse.com>
2022-05-24 01:14:15 +0530

    appointment and payment

--- src/app/pages/appointments/appointments.component.ts
+++ src/app/pages/appointments/appointments.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AyuService } from 'src/app/ayu.service';
+import { environment } from 'src/environments/environment';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -9,35 +13,81 @@ import Swal from 'sweetalert2';
 })
 export class AppointmentsComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  apiUrl = environment.backend_url;
+  channels:any=[]
+  p: number = 1; key:string=''; focus: any; focus2: any;
+  getAppointmentForDelete: any;
+  showAllData: any;
+  constructor(private router:Router,private modalService: NgbModal,private service: AyuService,private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.getChannels();
   }
 
-  editInfo(content1: any) {
-     this.modalService.open(content1, { centered: true });
-   }
-
-  cancelMessage(){
-    Swal.fire({
-      title: '<strong>Remove Appointment</strong>',
-      iconHtml: '<i class="fas fa-thumbs-down text-danger bg-white"></i>',
-      customClass: {
-        icon: 'no-border'
-      },
-      html:
-        'Do you want to Remove this Appointment?',
-      showCancelButton: true,
-      focusConfirm: false,
-      confirmButtonColor: '#D04848',
-      confirmButtonText:
-        'Remove',
-      cancelButtonText:
-        'Cancel',      
+  getChannels(){
+    this.http.get(this.apiUrl+'/channel/get-channels').subscribe(res=>{      
+      this.channels = res;
+      console.log(this.channels);
     })
   }
 
-  ErrorRemove () {
+  del_res:any;
+
+  deleteChannel(){
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    headers.append('accept', 'text/plain');
+    
+    this.http.get(this.apiUrl+'/channel/delete/'+this.getAppointmentForDelete
+    ,{
+      headers: headers,
+      reportProgress:true,
+      observe:'events',
+  
+    }
+    ).subscribe(res=>{
+      console.log(res);
+      this.del_res = res;
+      console.log(this.del_res.status);
+      if (this.del_res.status == 200){
+        //this.getMedicineById();
+        console.log("deleted")
+        this.SuccessMessage("Successfully removed the Channel");
+      }
+      else{
+        console.log('failed');
+        this.ErrorMessage("Channel Deletion failed");
+      }
+      this.modalService.dismissAll();
+      this.getChannels();
+    })
+  }
+
+  getAppointmentById(){
+    this.http.get(this.apiUrl+'/channel/get-channels/'+this.getAppointmentForDelete,{observe: 'response'}).subscribe(res=>{
+        console.warn(res);
+        this.getAppointmentForDelete = res;
+        if (this.getAppointmentForDelete.status === 0){
+          console.log("Error!! ");
+        }
+        else{
+          this.showAllData = this.getAppointmentForDelete.data;
+  
+        }
+    })
+  }
+
+  openDelete(content6: any,id: any) {
+    this.getAppointmentForDelete = id;
+    this.modalService.open(content6, { centered: true });
+  }
+
+
+  editInfo(content1: any,id:any) {
+     this.modalService.open(content1, { centered: true });
+   }
+
+   ErrorMessage (text:any) {
     const Toast = Swal.mixin({
       toast: true,
       position: 'top-end',
@@ -48,11 +98,11 @@ export class AppointmentsComponent implements OnInit {
     
     Toast.fire({
       icon: 'error',
-      title: "Remove Failed"
+      title: text
     })
   }
 
-  SuccessMessage () {
+  SuccessMessage (text:any) {
     const Toast = Swal.mixin({
       toast: true,
       position: 'top-end',
@@ -63,7 +113,7 @@ export class AppointmentsComponent implements OnInit {
     
     Toast.fire({
       icon: 'success',
-      title: "Removed Successfully"
+      title: text
     })
   }
 
